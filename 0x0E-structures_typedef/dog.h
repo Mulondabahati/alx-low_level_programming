1c2aa15572e3b84b2168db9de7f462f75a833244
Mulonda Bahati <Mulondabahati>
2023-09-11 08:12:04 -0700

    creates a struct for a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - a struct that creates a dof info
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: first struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
