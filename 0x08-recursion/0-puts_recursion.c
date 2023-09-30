00cbe43155f8a81d835bcb501b4771d98cddd79e
Mulonda Bahati <mulondabahatimichel@gmail.com>
2023-09-30 05:17:34 -0700

    hard for sure

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,13 @@
  */
 void _puts_recursion(char *s)
 {
+	if (*s != '\0')
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
 	if (*s == '\0')
 	{
 		putchar('\n');
 	}
-	putchar(*s);
-	_puts_recursion(s + 1);
 }
