889f7da82cc9ed2e6211f1991fbe3cd9e73ca2f6
Mulonda Bahati <mulondabahatimichel@gmail.com>
2023-10-03 02:14:04 -0700

    he is fun

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * @argc: number of command lines
+ * @argv: array that contains the number of command line
+ *
+ * Return: (0) if successfull
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
