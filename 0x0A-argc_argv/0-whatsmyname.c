f6597ed69ca989857a736f7d70e6b6d763d73e1e
Mulonda Bahati <mulondabahatimichel@gmail.com>
2023-10-03 02:08:41 -0700

    not bad

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * @argc: number of command lines
+ * @argv: array that contains the number of command line
+ *
+ * Return: (0) if successfull
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
