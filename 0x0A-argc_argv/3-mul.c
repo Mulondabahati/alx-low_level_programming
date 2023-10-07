d70c87d29780a1b0b66987008daec2ffe87d61ec
Mulonda Bahati <mulondabahatimichel@gmail.com>
2023-10-07 12:18:20 -0700

    makolo is now a programmer

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,17 +8,11 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	i = atoi(argv[1]) * atoi(argv[2]);
-	if (argc == 3)
-	{
-		printf("%d\n", i);
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	printf ("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
