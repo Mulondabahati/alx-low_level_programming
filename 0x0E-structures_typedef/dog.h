fb23392c3cc78b57792b36a13f5766acce8754d1
Mulonda Bahati <mulondabahatimichel@gmail.com>
2023-10-10 13:39:30 -0700

    moving on with struct def

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - a struct that has dog's info
+ * @name: name of the dog
+ * @age: it's age
+ * @owner: owner of the dog 
+ */
 struct dog
 {
 	char *name;
