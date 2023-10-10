470da7e784572341131bee2af1265dc83ee24ade
Mulonda Bahati <mulondabahatimichel@gmail.com>
2023-10-10 13:16:33 -0700

    the end

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef MACRO_H
+#define MACRO_H
+/**
+ * File - 4-sum.h
+ *
+ * @x: input value
+ * @y: input value
+ *
+ * Desc: header file for sum
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif/*4-sum.h*/
