9ce46a6b8417200bc5e3533e73a5148118783570
Mulonda Bahati <mulondabahatimichel@gmail.com>
2023-10-10 13:11:31 -0700

    absolute value

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef MACRO_H
+#define MACRO_H
+/**
+ * File - 3-function_like_macro.h
+ *
+ * Desc: header file
+ */
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif/*3-function_like_macro.h*/
+
