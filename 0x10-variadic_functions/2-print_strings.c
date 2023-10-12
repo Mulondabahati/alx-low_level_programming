4749006b7bed358909fb5d0c59e02669184bc999
Mulonda Bahati <mulondabahatimichel@gmail.com>
2023-10-12 14:32:39 -0700

    trying

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,21 +7,18 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	char *ptr;
 	va_list makolo;
 
 	va_start(makolo, n);
 	for (i = 0; i < n; i++)
 	{
-		ptr = va_arg(makolo, char *);
-
-		if (ptr == NULL)
+		if (n == '\0')
 		{
 			printf("(nil)");
 		}
 		else
 		{
-			printf("%s", ptr);
+			printf("%s", va_arg(makolo, char *));
 		}
 		if (i != (n - 1) && separator != NULL)
 		{
