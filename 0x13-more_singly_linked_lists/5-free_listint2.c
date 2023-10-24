ee793ec0a4dfc51e09936412dc2ebb7477513035
Mulonda Bahati <mulondabahatimichel@gmail.com>
2023-10-24 14:55:47 -0700

    free2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - free list
+ * @head: head
+ * Return: no
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head != NULL)
+	{
+	current = *head;
+	*head = (*head)->next;
+	free(current);
+	}
+
+	*head = NULL;
+}
