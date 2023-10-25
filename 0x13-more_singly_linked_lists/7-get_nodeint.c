4d7b818de155ff7c61f617c119e74edcc173d2d6
Mulonda Bahati <mulondabahatimichel@gmail.com>
2023-10-24 22:50:10 -0700

    at Alight

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,19 +1,17 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 /**
  * get_nodeint_at_index - makolo knows
  * @head: head
  * @index: index
  * Return: null
  */
-listint_t* get_nodeint_at_index(listint_t* head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int a;
 
 	for (a = 0; a < index; a++)
 	{
-		if (head== NULL)
+		if (head == NULL)
 			return (NULL);
 		head = head->next;
 	}
