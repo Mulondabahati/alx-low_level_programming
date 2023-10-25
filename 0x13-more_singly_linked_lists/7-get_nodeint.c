66d8bc5eceb86c734ef05870e34886e96bf9cddc
Mulonda Bahati <mulondabahatimichel@gmail.com>
2023-10-24 22:40:22 -0700

    at Alight

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,19 +9,13 @@
  */
 listint_t* get_nodeint_at_index(listint_t* head, unsigned int index)
 {
-	unsigned int count = 0;
-	listint_t* current;
+	unsigned int a;
 
-	current = head;
-	while (current != NULL)
+	for (a = 0; a < index; a++)
 	{
-	if (count == index)
-	{
-	return (current);
+		if (head== NULL)
+			return (NULL);
+		head = head->next;
 	}
-	count++;
-	current = current->next;
-	}
-
-	return (NULL);
+	return (head);
 }
