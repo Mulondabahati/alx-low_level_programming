bd9f3fb2edb432317fc89a0825777810ab155d70
Mulonda Bahati <mulondabahatimichel@gmail.com>
2023-10-25 13:31:52 -0700

    sum

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,7 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (a = 0; a < index; a++)
 	{
-		if (head == NULL)
+		if (head== NULL)
 			return (NULL);
 		head = head->next;
 	}
