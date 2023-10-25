08e0d497ed99e233521853fd173dc945ec3a58fa
Mulonda Bahati <mulondabahatimichel@gmail.com>
2023-10-24 22:28:00 -0700

    at Alight

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - makolo knows
+ * @head: head
+ * @index: index
+ * Return: null
+ */
+listint_t* get_nodeint_at_index(listint_t* head, unsigned int index)
+{
+	unsigned int count = 0;
+	listint_t* current;
+
+	current = head;
+	while (current != NULL)
+	{
+	if (count == index)
+	{
+	return (current);
+	}
+	count++;
+	current = current->next;
+	}
+
+	return (NULL);
+}
